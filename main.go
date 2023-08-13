b91a55e016cbaddf5391ef91a29758d6a8c72828
sadnessOjisan <sadness.ojisan@gmail.com>
2023-08-13 15:13:30 +0900

    impl simple server

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+// カウンターを持つ HTTP リクエストハンドラー
+type countHandler struct {
+	count int
+}
+
+func (h *countHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.count++
+	fmt.Fprintf(w, "Count: %d\n", h.count)
+}
 func main() {
-	fmt.Println("hello world")
+	http.Handle("/count", new(countHandler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
