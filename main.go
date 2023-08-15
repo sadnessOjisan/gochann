97a73716eb5da27c91f12f011ca54c3c04cea07f
sadnessOjisan <sadness.ojisan@gmail.com>
2023-08-15 22:58:08 +0900

    register user

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"crypto/sha256"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -21,7 +24,7 @@ func (h *countHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Count: %d\n", h.count)
 }
 
-type showUserHandler struct {
+type getUsersHandler struct {
 	count int
 }
 
@@ -33,33 +36,94 @@ type User struct {
 	UpdatedAt time.Time `db:"updated_at"`
 }
 
-func (h *showUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *getUsersHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		name := r.FormValue("name")
+		password := []byte(r.FormValue("password"))
+		hashedPassword := sha256.Sum256(password)
+
+		db, err := sql.Open("mysql", "ojisan:ojisan@(127.0.0.1:3306)/micro_post?parseTime=true")
+		if err != nil {
+			fmt.Printf("error")
+		}
+		ins, err := db.Prepare("insert into users(name, password) value (?, ?)")
+		if err != nil {
+			fmt.Printf("error")
+			return
+		}
+		defer db.Close()
+		ins.Exec(name, hashedPassword)
+		return
+	}
+	if r.Method == http.MethodGet {
+		db, err := sql.Open("mysql", "ojisan:ojisan@(127.0.0.1:3306)/micro_post?parseTime=true")
+		if err != nil {
+			fmt.Printf("error")
+		}
+		rows, err := db.Query("select * from users")
+
+		defer db.Close()
+
+		var users []User
+		for rows.Next() {
+			u := &User{}
+			if err := rows.Scan(&u.ID, &u.Name, &u.Email, &u.CreatedAt, &u.UpdatedAt); err != nil {
+				log.Fatalf("getRows rows.Scan error err:%v", err)
+			}
+			users = append(users, *u)
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(users); err != nil {
+			log.Println(err)
+		}
+	}
+
+}
+
+type getUserHandler struct {
+	count int
+}
+
+func (h *getUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Printf("method not allowed")
+		return
+	}
+	sub := strings.TrimPrefix(r.URL.Path, "/users")
+	_, id := filepath.Split(sub)
+	if id == "" {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Printf("id is not found")
+		return
+	}
 	db, err := sql.Open("mysql", "ojisan:ojisan@(127.0.0.1:3306)/micro_post?parseTime=true")
 	if err != nil {
 		fmt.Printf("error")
 	}
-	rows, err := db.Query("select * from users")
+	row := db.QueryRow("select * from users where id = ? limit 1", id)
 
 	defer db.Close()
 
-	var users []User
-	for rows.Next() {
-		u := &User{}
-		if err := rows.Scan(&u.ID, &u.Name, &u.Email, &u.CreatedAt, &u.UpdatedAt); err != nil {
-			log.Fatalf("getRows rows.Scan error err:%v", err)
-		}
-		users = append(users, *u)
+	u := &User{}
+	if err := row.Scan(&u.ID, &u.Name, &u.Email, &u.CreatedAt, &u.UpdatedAt); err != nil {
+		log.Fatalf("getRows rows.Scan error err:%v", err)
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(users); err != nil {
+	if err := json.NewEncoder(w).Encode(u); err != nil {
 		log.Println(err)
 	}
 }
 
 func main() {
 	http.Handle("/count", new(countHandler))
-	http.Handle("/users", new(showUserHandler))
+	http.Handle("/users", new(getUsersHandler))
+	// for /users/:id
+	http.Handle("/users/", new(getUserHandler))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
