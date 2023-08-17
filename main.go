dee33b238b9ec557e80893aa25108ad156050b6d
sadnessOjisan <sadness.ojisan@gmail.com>
2023-08-17 22:23:01 +0900

    show home page

--- main.go
+++ main.go
@@ -27,7 +27,6 @@ func pseudo_uuid() (uuid string) {
 	}
 
 	uuid = fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-
 	return
 }
 
@@ -55,7 +54,6 @@ type User struct {
 
 func (h *getUsersHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		println("POST")
 		name := r.FormValue("name")
 		password := []byte(r.FormValue("password"))
 		hasher := sha256.New()
@@ -387,10 +385,55 @@ func postsDetailHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, fmt.Sprintf("/posts/%s", post_id), http.StatusSeeOther)
 		return
 	}
+}
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	token, err := r.Cookie("token")
+	// cookie に token がないなら home ページを表示
+	if err != nil {
+		t := template.Must(template.ParseFiles("./template/home.html"))
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		if err := t.Execute(w, nil); err != nil {
+			log.Fatalf("tempalte engine rendering error err:%v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+
+	db, err := sql.Open("mysql", "ojisan:ojisan@(127.0.0.1:3306)/micro_post?parseTime=true")
+	defer db.Close()
+	if err != nil {
+		log.Fatalf("open db error err:%v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	row := db.QueryRow("select user_id from session where token = ? limit 1", token.Value)
+	var user_id int
+	if err := row.Scan(&user_id); err != nil {
+		log.Fatalf("user_id getRows rows.Scan error err:%v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// cookie の情報が session になかった場合
+	if user_id == 0 {
+		t := template.Must(template.ParseFiles("./template/home.html"))
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		if err := t.Execute(w, nil); err != nil {
+			log.Fatalf("tempalte engine rendering error err:%v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// user 情報が見つかった時
+	http.Redirect(w, r, "/posts", http.StatusSeeOther)
 }
 
 func main() {
+	http.HandleFunc("/", homeHandler)
 	http.Handle("/count", new(countHandler))
 	http.Handle("/users", new(getUsersHandler))
 	// for /users/:id
